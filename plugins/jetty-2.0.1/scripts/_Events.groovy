2a5346bc2976ffab97bb16839b37d2b95b3d8f16
Billy Yarosh <yaroshw1@gmail.com>
2012-10-06 00:40:10 -0400

    Remove plugins from versioning

--- plugins/jetty-2.0.1/scripts/_Events.groovy
+++
@@ -1,5 +0,0 @@
-// No programmable web.xml path yet, so put it in the right place automatically
-eventGenerateWebXmlEnd = {
-	packagePluginsForWar("${basedir}/web-app")
-	System.setProperty("grails.server.factory", "org.grails.jetty.JettyServerFactory")
-}
